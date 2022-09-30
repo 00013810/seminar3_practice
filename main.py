6d807dc4b76ea352768123523a6d68ee2e3b44cc
00013810 <wiut00013810>
2022-09-30 09:47:27 +0500

    internal commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# Seminar 3 Practice exercises
+
+# task 5
+# num = int(input('Print our number: '))
+# print(f"The {int(num)} is integer")
+# print(f"The {bin(num)} is binary")
+# print(f"The {oct(num)} is octagon")
+# print(f"The {hex(num)} is hexagon")
+
+# task 6
+# number = int(input('Print your number: '))
+# if number % 2 == 0:
+# print('This number is even')
+# else:
+# print('This number is odd')
+
+# def add(a, b):
+# return a + b
+# print(add(5, 6))
+
+# task 6 function
+# def odd_even(number):
+# return number % 2 == 0
+# print(odd_even(7))
+
+# task 5 function
+
+# def converter_num(number):
+# return (int(number)), bin(number), oct(number), hex(number)
+# print(converter_num(5))
